dea49f41089c8b0a1492680832257ef58c96f2a2
Rachid <info@addictedtoweb.com>
2023-09-21 08:37:17 +0100

    First commit

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Jobs functions and definitions
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Enqueue the style.css file.
+ *
+ * @since 1.0.0
+ */
+
+ add_action( 'wp_enqueue_scripts', 'jobs_enqueue_styles' );
+ function jobs_enqueue_styles() {
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/assets/styles/style.css');
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+ }
+
+/**
+ * Loads the translation files for WordPress.
+ *
+ * @since 1.0.0
+ */
+
+function jobs_theme_setup()
+{
+	load_child_theme_textdomain( 'jobs', get_stylesheet_directory() . '/languages' );
+}
+
+add_action('after_setup_theme', 'jobs_theme_setup');
